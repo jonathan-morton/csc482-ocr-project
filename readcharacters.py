b4a69fb4cd8e58ffb5d25ff04fd729fc4042f159
Jonathan Morton <djlarrikin@gmail.com>
2018-03-05 17:38:33 -0600

    Trying to get all characters

--- readcharacters.py
+++ readcharacters.py
@@ -4,14 +4,16 @@ from PIL import Image
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-#%%
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
 size_record = 8199
 file_count = 33
 root_filename = "ETL8G/ETL8G_"
 codes_filename = "jis208toUnicode.csv"
 #
 
-#%%
+
 class JisData:
     def __init__(self, codes_filename):
         self.df = self._get_jis_unicode_data_frame_(codes_filename)
@@ -28,19 +30,20 @@ class JisData:
         return '0x' + jis_code
 
     def get_character(self, jis_code):
+        jis_code = jis_code.upper()
         jis_query = f'JIS == "{jis_code}"'
         data = self.df.query(jis_query)
         kanji = data.iloc[0]['KANJI']
         return kanji
-#%%
+
 def get_all_jis_codes(japanese_chars):
     codes = []
     jis_data = JisData(codes_filename)
     for char in japanese_chars:
         codes.append(jis_data.get_jis_code(char))
     return codes
-#%%
-codes = get_all_jis_codes(["三","一","二","四","五","六","七","八", "九","十",])
+
+codes = get_all_jis_codes(["月"])
 
 #%%
 def read_record_ETL8G(f):
@@ -53,64 +56,58 @@ def read_record_ETL8G(f):
 def read_kanji(jis_codes):
     # Character type = 72, person = 160, y = 127, x = 128
     records = []
+    count = 0
+    jis_data = JisData(codes_filename)
+    charSet = set()
     last_file_count = 33 #33 files
     ary = np.zeros([len(jis_codes), 160, 127, 128], dtype=np.uint8)
-    for i in range(1,last_file_count):
+    for i in range(1,last_file_count+1):
         filename = root_filename + '{:02d}'.format(i)
         with open(filename, 'rb') as f:
-            for id_dataset in range(5):
+            if i == last_file_count:
+                id_range = range(1)
+            else:
+                id_range = range(5)
+            for id_dataset in id_range:
+                f.seek(id_dataset * 956 * size_record)
                 for j in range(956):
                     record = read_record_ETL8G(f)
+                    #print(jis_data.get_character(hex(record[1])[2:]))
                     if(hex(record[1]) in jis_codes):
+                        charSet.add(hex(record[1])[2:])
                         record_list = list(record)
                         kanji_image = Image.eval(record[-1], lambda x: 255 - x * 16)
                         kanji_np_array = np.array(kanji_image)
                         record_list[-1] = kanji_np_array
                         new_record = tuple(record_list)
                         records.append(new_record)
-    return records
+    return charSet, records
 
-records = read_kanji(codes)
+charSet, records = read_kanji(codes)
 #%%
+import imageprocessing
+import model
+#%%
+large_image = records[30][-1]
+small_image = imageprocessing.process_image(large_image)
+plt.interactive(True)
+plt.imshow(small_image, cmap='gray') # https://intellij-support.jetbrains.com/hc/en-us/community/posts/115000143610-Problems-with-Interactive-Plotting-in-Debug-Mode-in-PyCharm-Version-2017-1
+#%%
+from keras.utils import to_categorical
+
 def test_train_data(records):
     np.random.shuffle(records)
     X = [record[-1] for record in records]
+    X = np.asarray(X)
+
     Y = [record[1] for record in records]
     train_x, train_y, test_x, test_y = train_test_split(X, Y, test_size=0.25)
+
     return train_x, train_y, test_x, test_y
 
 train_x, train_y, test_x, test_y = test_train_data(records)
+# train_y = to_categorical(train_y)
+# test_y = to_categorical(test_y)
 #%%
-def test_etlcdb():
-    filename = 'ETL8G/ETL8G_01'
-    id_record = 0
-
-    with open(filename, 'rb') as f:
-        r = read_record_ETL8G(f)
-
-    print(r[0:-2], hex(r[1]))
-    iE = Image.eval(r[-1], lambda x: 255 - x * 16)
-    fn = 'ETL8G_{:d}_{:s}.png'.format((r[0] - 1) % 20 + 1, hex(r[1])[-4:])
-    iE.save(fn, 'PNG')
-
-def read_hiragana():
-    # Character type = 72, person = 160, y = 127, x = 128
-    ary = np.zeros([72, 160, 127, 128], dtype=np.uint8)
-
-    for j in range(1, 33):
-        filename = 'ETL8G/ETL8G_{:02d}'.format(j)
-        with open(filename, 'rb') as f:
-            for id_dataset in range(5):
-                moji = 0
-                for i in range(956):
-                    r = read_record_ETL8G(f)
-                    if b'.HIRA' in r[2]:
-                        ary[moji, (j - 1) * 5 + id_dataset] = np.array(r[-1])
-                        moji += 1
-    print(ary)
-    #np.savez_compressed("hiragana.npz", ary)
-
-
-read_hiragana()
 
 #test_etlcdb()
