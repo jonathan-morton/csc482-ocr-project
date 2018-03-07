553ce8c283acc590cdcf02351612c59c99e4bb3b
Jonathan Morton <djlarrikin@gmail.com>
2018-03-06 23:32:26 -0600

    Decent model with extra metrics

--- cnn_model.py
+++ cnn_model.py
@@ -1,9 +1,30 @@
 __author__ = 'Jonathan Morton'
-from keras import models
+import numpy as np
 from keras import layers
+from keras import models
 from keras import optimizers
-from keras import losses
-from keras import regularizers
+from keras.callbacks import Callback
+from sklearn.metrics import precision_score, recall_score
+
+
+# From: https://medium.com/@thongonary/how-to-compute-f1-score-for-each-epoch-in-keras-a1acd17715a2
+class Metrics(Callback):
+
+    def on_train_begin(self, logs={}):
+        self.val_f1s = []
+        self.val_recalls = []
+        self.val_precisions = []
+
+    def on_epoch_end(self, epoch, logs={}):
+        val_predict = (np.asarray(self.model.predict(self.validation_data[0]))).round()
+        val_targ = self.validation_data[1]
+        _val_recall = recall_score(val_targ, val_predict, average=None)
+        _val_precision = precision_score(val_targ, val_predict, average=None)
+        self.val_recalls.append(_val_recall)
+        self.val_precisions.append(_val_precision)
+        print(f" — val_precision: {_val_precision} — val_recall {_val_recall}")
+        return
+
 
 def get_cnn_model(image_size, classification_num):
     model = models.Sequential()
@@ -18,6 +39,7 @@ def get_cnn_model(image_size, classification_num):
     model.add(layers.Dense(classification_num, activation='softmax'))
 
     # sgd = optimizers.SGD(lr=0.1, decay=1e-6, momentum=1.9)
+    metrics = Metrics()
     model.compile(optimizers.Adam(lr=1e-5), 'categorical_crossentropy', metrics=['accuracy'])
 
     return model
